7b7d6f2aaaa1b4b0120e65e128945d9a33f2b2bf
Ayal Ciobotaru <ayal.ciobotaru@gmail.com>
2025-10-19 05:48:56 -0400

    added free-agent endpoint

---
+++ api/free-agent.js
@@ -0,0 +1,29 @@
+// api/chrva.js
+import fetch from "node-fetch";
+
+const GOOGLE_SCRIPT_URL = process.env.GS_FREE_AGENT_URL;
+
+export default async function handler(req, res) {
+  // Set CORS headers for every response
+  res.setHeader("Access-Control-Allow-Origin", "https://www.chrva.org");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  // Handle preflight
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+
+  try {
+    console.log("Env Script URL: " + GOOGLE_SCRIPT_URL)
+    const response = await fetch(GOOGLE_SCRIPT_URL);
+    const data = await response.text();
+ 
+    res.status(200).send(data);
+  } catch (error) {
+    console.error("Proxy error:", error);
+    res.status(500).json({ error: error.message });
+  }
+}
